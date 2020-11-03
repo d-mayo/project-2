fd5b6bfb1e5d34e741d5f798079855196c42ca70
Lawrence DeMaio <ldemaio121@hotmail.com>
2020-11-03 15:52:02 -0500

    Add express session

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
 const app = express();
+const session = require('express-session')
 const db = mongoose.connection;
 //___________________
 //Port
@@ -16,6 +17,8 @@ const PORT = process.env.PORT || 3000;
 //___________________
 // How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ 'PROJECT-2';
+// set secret thru heroku or seperate file
+const SECRET = process.env.SECRET || require('./secret.js');
 // Connect to Mongo
 mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
 // Error / success
@@ -36,9 +39,20 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
+app.use(
+    session({
+      secret: SECRET, //a random string do not copy this value or your stuff will get hacked
+      resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
+      saveUninitialized: false // default  more info: https://www.npmjs.com/package/express-session#resave
+    })
+  )
+
 // Controllers
 const usersController = require('./controllers/users_controller.js')
 app.use('/users', usersController)
+
+const ingredientsController = require('./controllers/ingredients_controller.js')
+app.use('/ingredients', ingredientsController)
 
 const sessionsController = require('./controllers/sessions_controller.js')
 app.use('/sessions', sessionsController)
